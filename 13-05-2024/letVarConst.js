72441d7f03c0bd699301aac6ecf53b94ddee4539
Satish Rathod <satishrathod.dev@gmail.com>
2024-05-19 12:56:38 +0530

    variableDeclarations

---
+++ 13-05-2024/letVarConst.js
@@ -0,0 +1,14 @@
+// let a = 10;
+// let b = 20;
+// let sum = 20;
+
+function hello() {
+    function displayAge() {
+        console.log(age);
+
+    }
+    let age = 20;
+    displayAge();
+}
+hello();
+// displayAge();
